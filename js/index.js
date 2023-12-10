0e25b11847c2b3908b42551600653af3c8bf566f
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-12-10 19:03:26 +0100

    add sphere and earth texture to sphere. Also add space background

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ import { OrbitControls } from "https://threejs.org/examples/jsm/controls/OrbitCo
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -11,10 +14,28 @@ const controls = new OrbitControls( camera, renderer.domElement );
 
 const cubeColor = new THREE.Color("Orange")
 
-const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
-const cubeMaterial = new THREE.MeshBasicMaterial( { color: cubeColor } );
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
-scene.add( cube );
+// const cubeGeometry = new THREE.BoxGeometry( 8, 0.5, 8 );
+// const cubeMaterial = new THREE.MeshBasicMaterial( { color: cubeColor } );
+// const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+// scene.add( cube );
+
+const sphereTexture = new THREE.TextureLoader().load('textures/earth1.jpg')
+
+const geometry = new THREE.SphereGeometry( 1, 40, 20 ); 
+const material = new THREE.MeshBasicMaterial( { map: sphereTexture } ); 
+const sphere = new THREE.Mesh( geometry, material ); 
+scene.add( sphere );
+
+scene.background = new THREE.CubeTextureLoader()
+	.setPath( 'textures/' )
+	.load( [
+				'space.png',
+				'space.png',
+				'space.png',
+				'space.png',
+				'space.png',
+				'space.png'
+			] );
 
 camera.position.z = 2
 controls.update();	
