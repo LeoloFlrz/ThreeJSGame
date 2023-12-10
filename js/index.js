4fd88047e4e324e48105ff0844e924a31ff0dfd5
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-12-10 19:31:08 +0100

    created floor with lights using functions for it

--- js/index.js
+++ js/index.js
@@ -14,26 +14,14 @@ const controls = new OrbitControls( camera, renderer.domElement );
 
 const cubeColor = new THREE.Color("Orange")
 
+camera.position.y = 2
+
+generateFloor()
 // const cubeGeometry = new THREE.BoxGeometry( 8, 0.5, 8 );
 // const cubeMaterial = new THREE.MeshBasicMaterial( { color: cubeColor } );
 // const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
 // scene.add( cube );
 
-const sphereTexture = new THREE.TextureLoader().load('textures/earth1.jpg')
-const moonTexture = new THREE.TextureLoader().load('textures/moon.jpg')
-
-const geometry = new THREE.SphereGeometry( 1, 40, 20 ); 
-const material = new THREE.MeshBasicMaterial( { map: sphereTexture } ); 
-const sphere = new THREE.Mesh( geometry, material ); 
-scene.add( sphere );
-
-const moonGeometry = new THREE.SphereGeometry( 0.2, 40, 20 ); 
-const moonMaterial = new THREE.MeshBasicMaterial( { map: moonTexture } ); 
-const moon = new THREE.Mesh( moonGeometry, moonMaterial ); 
-scene.add( moon );
-
-moon.position.x = 3
-moon.position.y = 1
 
 scene.background = new THREE.CubeTextureLoader()
 	.setPath( 'textures/' )
@@ -57,3 +45,59 @@ function animate() {
 }
 
 animate();
+
+
+function generateFloor() {
+    // TEXTURES
+    const textureLoader = new THREE.TextureLoader();
+    const placeholder = textureLoader.load("./textures/placeholder/placeholder.png");
+    const sandBaseColor = textureLoader.load("./textures/sand/Sand 002_COLOR.jpg");
+    const sandNormalMap = textureLoader.load("./textures/sand/Sand 002_NRM.jpg");
+    const sandHeightMap = textureLoader.load("./textures/sand/Sand 002_DISP.jpg");
+    const sandAmbientOcclusion = textureLoader.load("./textures/sand/Sand 002_OCC.jpg");
+
+    const WIDTH = 80
+    const LENGTH = 80
+
+    const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
+    const material = new THREE.MeshStandardMaterial(
+        {
+            map: sandBaseColor, normalMap: sandNormalMap,
+            displacementMap: sandHeightMap, displacementScale: 0.1,
+            aoMap: sandAmbientOcclusion
+        })
+    wrapAndRepeatTexture(material.map)
+    wrapAndRepeatTexture(material.normalMap)
+    wrapAndRepeatTexture(material.displacementMap)
+    wrapAndRepeatTexture(material.aoMap)
+    // const material = new THREE.MeshPhongMaterial({ map: placeholder})
+
+    const floor = new THREE.Mesh(geometry, material)
+    floor.receiveShadow = true
+    floor.rotation.x = - Math.PI / 2
+    scene.add(floor)
+
+	function wrapAndRepeatTexture(map) {
+		map.wrapS = map.wrapT = THREE.RepeatWrapping;
+		map.repeat.x = map.repeat.y = 10;
+	}
+
+	function light() {
+		scene.add(new THREE.AmbientLight(0xffffff, 0.7))
+	
+		const dirLight = new THREE.DirectionalLight(0xffffff, 1)
+		dirLight.position.set(- 60, 100, - 10);
+		dirLight.castShadow = true;
+		dirLight.shadow.camera.top = 50;
+		dirLight.shadow.camera.bottom = - 50;
+		dirLight.shadow.camera.left = - 50;
+		dirLight.shadow.camera.right = 50;
+		dirLight.shadow.camera.near = 0.1;
+		dirLight.shadow.camera.far = 200;
+		dirLight.shadow.mapSize.width = 4096;
+		dirLight.shadow.mapSize.height = 4096;
+		scene.add(dirLight);
+		// scene.add( new THREE.CameraHelper(dirLight.shadow.camera))
+	}
+	light()
+}
